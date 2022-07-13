c2aef43f0ad0decf12849d99e9f28b124375f91e
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2022-07-12 22:33:07 -0300

    v0.1 - menu com filtro de imagens.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Arquivos SASS
+import './scss/index.scss'
+
+// Depedencias importadas
+import 'jquery'
+import 'bootstrap'
+
+// Importação dos meu arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
